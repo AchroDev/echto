368d02c91ea5d2763bd0c1edd3a03acc5ac6deb4
AchroDev <ztblaine@protonmail.com>
2024-05-12 00:30:43 -0500

    Switched to proper error handling

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,27 @@ fn to_ctrl_byte(c: char) -> u8 {
     byte & 0b0001_1111
 }
 
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
+
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b, c);
-        }
-        if b == to_ctrl_byte('q') {
-            break;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?}\r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
         }
     }
 }
