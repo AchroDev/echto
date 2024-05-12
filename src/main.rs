74e4373231a6f311cbba48e5be98535484cc317d
AchroDev <ztblaine@protonmail.com>
2024-05-12 00:09:32 -0500

    Added termion and changed the way input was received and output is displayed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b, c);
+        }
+        if c == 'q' {
+            break;
+        }
+    }
 }
