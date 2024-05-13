d052d3ef90d68a81cc3483aa7a3df1db7ed57ff5
AchroDev <ztblaine@protonmail.com>
2024-05-13 01:40:31 -0500

    Adjusted code for welcome_message so it doesn't get cut off

--- src/editor.rs
+++ src/editor.rs
@@ -70,7 +70,10 @@ impl Editor {
         for row in 0..height - 1 {
             Terminal::clear_current_line();
             if row == height / 3 {
-                println!("Echto text editor -- version {}\r", VERSION)
+                let welcome_message = format!("Echto text editor -- version {}\r", VERSION);
+                let width =
+                    std::cmp::min(self.terminal.size().width as usize, welcome_message.len());
+                println!("{}\r", &welcome_message[..width]);
             } else {
                 println!("~\r");
             }
