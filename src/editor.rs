2b6a8c04ee5c322d9acab6a9e22e095b17e901b4
AchroDev <ztblaine@protonmail.com>
2024-05-13 00:25:23 -0500

    Refactored code moving from editor.rs to terminal.rs

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,5 @@
 use crate::Terminal;
-use std::io::{self, stdout, Write};
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 
 // Struct for defining the text editors configuration
 pub struct Editor {
@@ -14,9 +11,6 @@ pub struct Editor {
 impl Editor {
     // Allows this mod to be called ("ran") in main.rs with .run()
     pub fn run(&mut self) {
-        // Sets the terminal mode to raw instead of canonical
-        let _stdout = stdout().into_raw_mode().unwrap();
-
         // Main loop during runtime
         loop {
             // Run fn refresh_screen and if there is an error, die with &error
@@ -45,19 +39,20 @@ impl Editor {
 
     // Refreshes the screen on open and exit, also resetting the cursor position to the top left.
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        Terminal::clear_screen();
+        Terminal::cursor_position(0, 0);
         if self.should_quit {
             println!("Thanks for using Echto!\r");
         } else {
             self.draw_rows();
-            print!("{}", termion::cursor::Goto(1, 1));
+            Terminal::cursor_position(0, 0);
         }
-        io::stdout().flush()
+        Terminal::flush()
     }
 
     // Performs action based on read keypress
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
             _ => (),
@@ -73,17 +68,8 @@ impl Editor {
     }
 }
 
-// Reads the key being pressed
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
-}
-
 // Defines how the program should crash
 fn die(e: &std::io::Error) {
-    print!("{}", termion::clear::All);
+    Terminal::clear_screen();
     panic!("{}", e);
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,3 +1,8 @@
+use std::io::{self, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+
 // Helper Struct that defines the size of the terminal
 pub struct Size {
     pub width: u16,
@@ -7,6 +12,8 @@ pub struct Size {
 // Struct for defining the Terminal configuration
 pub struct Terminal {
     size: Size,
+    // Sets the terminal mode to raw instead of canonical
+    _stdout: RawTerminal<std::io::Stdout>,
 }
 
 // Implementation of Terminal struct
@@ -22,11 +29,38 @@ impl Terminal {
                 width: size.0,
                 height: size.1,
             },
+            _stdout: stdout().into_raw_mode()?,
         })
     }
 
     // Declares the size to be returned as a read-only reference to our internal size
     pub fn size(&self) -> &Size {
         &self.size
+    }
+
+    // Clears the screen when called
+    pub fn clear_screen() {
+        print!("{}", termion::clear::All);
+    }
+
+    // Resets the cursor position when called
+    pub fn cursor_position(x: u16, y: u16) {
+        let x = x.saturating_add(1);
+        let y = y.saturating_add(1);
+        print!("{}", termion::cursor::Goto(1, 1));
+    }
+
+    // When called, forces stdout to print out everything it has, either wrapping nothing or erroring
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    // Reads the key being pressed
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
     }
 }
