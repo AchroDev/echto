a81533b9c57993582f297d9b54ed0584976c13d5
AchroDev <achrodev@proton.me>
2024-05-17 22:24:42 -0500

    Updated method refresh_screen to subtract the offset from the cursor position within the document, so that is stays properly positioned

--- src/editor.rs
+++ src/editor.rs
@@ -71,7 +71,10 @@ impl Editor {
             println!("Thanks for using Echto! - AchroDev\r");
         } else {
             self.draw_rows();
-            Terminal::cursor_position(&self.cursor_position);
+            Terminal::cursor_position(&Position {
+                x: self.cursor_position.x.saturating_sub(self.offset.x),
+                y: self.cursor_position.y.saturating_sub(self.offset.y),
+            });
         }
         Terminal::cursor_show();
         Terminal::flush()
