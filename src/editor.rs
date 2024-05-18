0a58850e5dc224bd5f420f991cd23e1f6f3d377f
AchroDev <achrodev@proton.me>
2024-05-17 22:32:52 -0500

    Fixed horizontal scrolling past the right of screen.

--- src/editor.rs
+++ src/editor.rs
@@ -122,9 +122,12 @@ impl Editor {
     // Moves the cursor along the x and y axis depending on which arrow key is used
     fn move_cursor(&mut self, key: Key) {
         let Position { mut y, mut x } = self.cursor_position;
-        let size = self.terminal.size();
         let height = self.document.len();
-        let width = size.width.saturating_add(1) as usize;
+        let width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
         match key {
             Key::Up => y = y.saturating_sub(1),
             Key::Down => {
