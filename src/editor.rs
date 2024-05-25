152f387e1caf0c39cc8cbaabffd3f0c5cbdf552e
AchroDev <achrodev@proton.me>
2024-05-25 01:15:43 -0500

    Added method prompt to accept a prompt from the user and enter a filename if the document doesn't have a name.

--- src/editor.rs
+++ src/editor.rs
@@ -117,6 +117,9 @@ impl Editor {
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
             Key::Ctrl('s') => {
+                if self.document.file_name.is_none() {
+                    self.document.file_name = Some(self.prompt("Save as: ")?);
+                }
                 if self.document.save().is_ok() {
                     self.status_message =
                         StatusMessage::from("File saved successfully".to_string());
@@ -308,6 +311,25 @@ impl Editor {
             text.truncate(self.terminal.size().width as usize);
             print!("{}", text);
         }
+    }
+
+    // Accepts a prompt from the user
+    fn prompt(&mut self, prompt: &str) -> Result<String, std::io::Error> {
+        let mut result = String::new();
+        loop {
+            self.status_message = StatusMessage::from(format!("{}{}", prompt, result));
+            self.refresh_screen()?;
+            if let Key::Char(c) = Terminal::read_key()? {
+                if c == '\n' {
+                    self.status_message = StatusMessage::from(String::new());
+                    break;
+                }
+                if !c.is_control() {
+                    result.push(c);
+                }
+            }
+        }
+        Ok(result)
     }
 }
 
