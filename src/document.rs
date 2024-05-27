f301061261f8587ca4ba6b1e9d1c6d29cd0cf112
AchroDev <achrodev@proton.me>
2024-05-27 01:00:05 -0500

    Replaced unwrapping get_mut() with direct access by self.rows

--- src/document.rs
+++ src/document.rs
@@ -38,21 +38,22 @@ impl Document {
 
     // Inserts a new line at given position
     fn insert_newline(&mut self, at: &Position) {
-        if at.y > self.len() {
+        if at.y > self.rows.len() {
             return;
         }
-        if at.y == self.len() {
+        if at.y == self.rows.len() {
             self.rows.push(Row::default());
             return;
         }
-        let new_row = self.rows.get_mut(at.y).unwrap().split(at.x);
+        #[allow(clippy::indexing_slicing)]
+        let new_row = self.rows[at.y].split(at.x);
         #[allow(clippy::arithmetic_side_effects)]
         self.rows.insert(at.y + 1, new_row);
     }
 
     // Inserts a character at the given position
     pub fn insert(&mut self, at: &Position, c: char) {
-        if at.y > self.len() {
+        if at.y > self.rows.len() {
             return;
         }
         self.dirty = true;
@@ -60,30 +61,31 @@ impl Document {
             self.insert_newline(at);
             return;
         }
-        if at.y == self.len() {
+        if at.y == self.rows.len() {
             let mut row = Row::default();
             row.insert(0, c);
             self.rows.push(row);
-        } else if at.y < self.len() {
-            let row = self.rows.get_mut(at.y).unwrap();
+        } else {
+            #[allow(clippy::indexing_slicing)]
+            let row = &mut self.rows[at.y];
             row.insert(at.x, c);
         }
     }
 
-    #[allow(clippy::arithmetic_side_effects)]
+    #[allow(clippy::arithmetic_side_effects, clippy::indexing_slicing)]
     // Deletes character at given position
     pub fn delete(&mut self, at: &Position) {
-        let len = self.len();
+        let len = self.rows.len();
         if at.y >= len {
             return;
         }
         self.dirty = true;
-        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y + 1 < len {
+        if at.x == self.rows[at.y].len() && at.y + 1 < len {
             let next_row = self.rows.remove(at.y + 1);
-            let row = self.rows.get_mut(at.y).unwrap();
+            let row = &mut self.rows[at.y];
             row.append(&next_row);
         } else {
-            let row = self.rows.get_mut(at.y).unwrap();
+            let row = &mut self.rows[at.y];
             row.delete(at.x);
         }
     }
