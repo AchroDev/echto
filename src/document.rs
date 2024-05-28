2a326ff7f5e1c27b7937bf589543d09692f30300
AchroDev <achrodev@proton.me>
2024-05-27 22:43:26 -0500

    Fixed parameter in if block for find method

--- src/document.rs
+++ src/document.rs
@@ -117,7 +117,7 @@ impl Document {
     pub fn find(&self, query: &str, after: &Position) -> Option<Position> {
         let mut x = after.x;
         for (y, row) in self.rows.iter().enumerate().skip(after.y) {
-            if let Some(x) = row.find(query) {
+            if let Some(x) = row.find(query, x) {
                 return Some(Position { x, y });
             }
         }
