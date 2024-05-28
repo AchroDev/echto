2c4110b44a7c2fc03a66b445d135099c01e6a361
AchroDev <achrodev@proton.me>
2024-05-28 00:47:25 -0500

    Searching backwards

--- src/editor.rs
+++ src/editor.rs
@@ -13,6 +13,12 @@ const STATUS_FG_COLOR: color::Rgb = color::Rgb(0, 0, 0);
 const STATUS_BG_COLOR: color::Rgb = color::Rgb(0, 170, 170);
 const QUIT_TIMES: u8 = 3;
 
+#[derive(PartialEq, Clone, Copy)]
+pub enum SearchDirection {
+    Forward,
+    Backward,
+}
+
 // Struct for tracking the cursors x and y position
 #[derive(Default, Clone)]
 pub struct Position {
@@ -137,19 +143,26 @@ impl Editor {
     // Updates the prompt in StatusMessage and updates the cursor position based on the current query
     fn search(&mut self) {
         let old_position = self.cursor_position.clone();
-        if let Some(query) = self
+        let mut direction = SearchDirection::Forward;
+        let query = self
             .prompt(
                 "Search (ESC to cancel, Arrow keys to navigate): ",
                 |editor, key, query| {
                     let mut moved = false;
                     match key {
                         Key::Right | Key::Down => {
+                            direction = SearchDirection::Forward;
                             editor.move_cursor(Key::Right);
                             moved = true;
                         }
-                        _ => (),
+                        Key::Left | Key::Up => direction = SearchDirection::Backward,
+                        _ => direction = SearchDirection::Forward,
                     }
-                    if let Some(position) = editor.document.find(&query, &editor.cursor_position) {
+                    if let Some(position) =
+                        editor
+                            .document
+                            .find(&query, &editor.cursor_position, direction)
+                    {
                         editor.cursor_position = position;
                         editor.scroll();
                     } else if moved {
@@ -157,14 +170,8 @@ impl Editor {
                     }
                 },
             )
-            .unwrap_or(None)
-        {
-            if let Some(position) = self.document.find(&query[..], &old_position) {
-                self.cursor_position = position;
-            } else {
-                self.status_message = StatusMessage::from(format!("Not found: {}", query));
-            }
-        } else {
+            .unwrap_or(None);
+        if query.is_none() {
             self.cursor_position = old_position;
             self.scroll();
         }
@@ -392,9 +399,9 @@ impl Editor {
     }
 
     // Accepts a prompt from the user
-    fn prompt<C>(&mut self, prompt: &str, callback: C) -> Result<Option<String>, std::io::Error>
+    fn prompt<C>(&mut self, prompt: &str, mut callback: C) -> Result<Option<String>, std::io::Error>
     where
-        C: Fn(&mut Self, Key, &String),
+        C: FnMut(&mut Self, Key, &String),
     {
         let mut result = String::new();
         loop {
