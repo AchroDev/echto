8a8fe98f21b77e5eb53293cf0258159febcff551
AchroDev <achrodev@proton.me>
2024-05-29 00:03:36 -0500

    Calling highlight everywhere in document.rs where we modify a row then refactored the rendering of a row to enumerate the grapheme to get the correct highlighting.

--- src/document.rs
+++ src/document.rs
@@ -3,6 +3,7 @@ use crate::Row;
 use crate::SearchDirection;
 use std::fs;
 use std::io::{Error, Write};
+use std::thread::current;
 
 #[derive(Default)]
 pub struct Document {
@@ -16,7 +17,9 @@ impl Document {
         let contents = fs::read_to_string(filename)?;
         let mut rows = Vec::new();
         for value in contents.lines() {
-            rows.push(Row::from(value));
+            let mut row = Row::from(value);
+            row.highlight();
+            rows.push(row);
         }
         Ok(Self {
             rows,
@@ -50,7 +53,10 @@ impl Document {
             return;
         }
         #[allow(clippy::indexing_slicing)]
-        let new_row = self.rows[at.y].split(at.x);
+        let current_row = &mut self.rows[at.y];
+        let mut new_row = current_row.split(at.x);
+        current_row.highlight();
+        new_row.highlight();
         #[allow(clippy::arithmetic_side_effects)]
         self.rows.insert(at.y + 1, new_row);
     }
@@ -68,11 +74,13 @@ impl Document {
         if at.y == self.rows.len() {
             let mut row = Row::default();
             row.insert(0, c);
+            row.highlight();
             self.rows.push(row);
         } else {
             #[allow(clippy::indexing_slicing)]
             let row = &mut self.rows[at.y];
             row.insert(at.x, c);
+            row.highlight();
         }
     }
 
@@ -88,9 +96,11 @@ impl Document {
             let next_row = self.rows.remove(at.y + 1);
             let row = &mut self.rows[at.y];
             row.append(&next_row);
+            row.highlight();
         } else {
             let row = &mut self.rows[at.y];
             row.delete(at.x);
+            row.highlight();
         }
     }
 
